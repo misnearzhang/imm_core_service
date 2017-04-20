2f01bc72d7165501f1c8613056444857fc129d05
Misnearzhang <misnearzhang@gmail.com>
2017-04-20 11:52:29 +0800

    format the code

--- src/main/java/im/core/executor/ThreadPool.java
+++ src/main/java/im/core/executor/ThreadPool.java
@@ -2,6 +2,7 @@ package im.core.executor;
 
 import im.config.SystemConfig;
 import io.netty.channel.Channel;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.concurrent.*;
 
@@ -11,7 +12,6 @@ import java.util.concurrent.*;
  * @author Misnearzhang
  */
 public class ThreadPool {
-
     public enum RetransCount {
         FISRT, SECOND, THIRD
     }
@@ -20,7 +20,7 @@ public class ThreadPool {
     /**
      * parse线程池，corePoolSize maximumPoolSize keepAliveTime TimeUnit queue
      */
-    private static ThreadPoolExecutor parser = new ThreadPoolExecutor(SystemConfig.threadCorePoolSize, SystemConfig.threadMaximumPoolSize, SystemConfig.threadKeepAliveTime, TimeUnit.SECONDS, queue);
+    private static ThreadPoolExecutor parser = new ThreadPoolExecutor(10, 20, 5, TimeUnit.SECONDS, queue);
 
     /**
      * 发送线程池
@@ -55,7 +55,7 @@ public class ThreadPool {
      * @param uid
      */
     public static void sendMessage(SendTask task, String uid) {
-        ScheduledFuture future = retransExecutor.schedule(task, SystemConfig.threadRetransTime, TimeUnit.SECONDS);
+        ScheduledFuture future = retransExecutor.schedule(task, 3, TimeUnit.SECONDS);
         futures.put(uid, future);
 
     }
