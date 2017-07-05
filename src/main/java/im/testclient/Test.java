9f28bf9ab1ab6a7b8b0605e6c5d07d7c4f7e061f
zhanglong <misnearzhang@gmail.com>
2017-07-05 17:23:39 +0800

    change something

--- src/main/java/im/testclient/Test.java
+++ src/main/java/im/testclient/Test.java
@@ -2,6 +2,7 @@ package im.testclient;
 
 import im.process.ParseTask;
 
+import java.net.Socket;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Predicate;
@@ -16,114 +17,46 @@ import static java.lang.Long.reverseBytes;
  * Created by misnearzhang on 2017/5/10.
  */
 public class Test {
+    private static volatile boolean isShutDown=true;
     public static void main(String[] args) {
-        /*long start = System.currentTimeMillis();
-        for(int i=0;i<10000;i++){
-            String s= UUID.randomUUID().toString();
-            System.out.println(s);
+        Integer[] index = {10,555,888,10000,66661,23,999,2,6423,9977,211,9990};
+        ArrayList<String> A = new ArrayList<>(10000000);
+
+        for (int i = 0; i < 10000000; i++) {
+            A.add("hello"+i);
         }
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);*/
-        /*int m=4323;
-        System.out.println(m&0xffffffff);
-        System.out.println((byte)(m>>24)&0xff);
-        System.out.println((byte)(m>>16)&0xff);
-        System.out.println((byte)(m>>8)&0xff);
-        System.out.println((byte)(m>>0)&0xff);
-        ArrayList<Message> list = new ArrayList<>(100);
-        for (int i = 0; i <100; i++) {
-            Message message = new Message();
-            message.setAge(10);
-            message.setUuid(UUID.randomUUID().toString());
-            message.setUserName("");
-            message.setPassword("");
-            message.setSex("");
-            list.add(message);
-        }*/
-        /*String inedxUID= "";
-        ArrayList<Message> userList = new ArrayList<Message>(1000000);
-        for (int i = 0; i < 1000000; i++) {
-            Message message = new Message(i);
-            userList.add(message);
-            if(i==50000){
-                inedxUID = message.getUuid();
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < index.length; i++) {
+            A.remove(index[i]);
+        }
+        System.out.println(System.currentTimeMillis()-start);
+
+        /*Arrays.sort(index);
+        System.out.println(Arrays.toString(index));
+        long start = System.currentTimeMillis();
+        ArrayList<String> B = new ArrayList<>(10000000-index.length);
+        int pos=0;
+        for(int i=0;i<index.length;i++){
+            int length=0;
+            if(i==0){
+                length=index[0];
+            }else{
+                length=index[i]-index[i-1];
+            }
+            if(i==0){
+                B.addAll(0,A.subList(0,index[i]));
+                //System.arraycopy(A,0,B,0,length);
+            }else{
+                //System.arraycopy(A,index[i],B,pos,length);
+                B.addAll(pos,A.subList(index[i-1]+1,index[i]));
+            }
+            pos+=length-1;
+            if(i==index.length-1){
+                B.addAll(pos-1,A.subList(index[i]+1,A.size()));
             }
         }
-        Long start = System.currentTimeMillis();
-        String finalInedxUID = inedxUID;
-        List<Message> collect = userList.stream().filter(p->p.getUuid().equals(finalInedxUID)).collect(Collectors.toList());
-        System.out.println(collect);
         System.out.println(System.currentTimeMillis()-start);*/
-
-        String Parse_String  = ParseTask.class.getName();
-        System.out.println(Parse_String);
-    }
-
-
-    public static class Message{
-        private String uuid;
-        private String userName;
-        private String password;
-        private String sex;
-        private Integer age;
-
-        public Message(int index) {
-            this.uuid = UUID.randomUUID().toString();
-            this.userName = "zhanglong"+index;
-            this.password = "";
-            this.sex = "";
-            this.age = index;
-        }
-
-        public String getUuid() {
-            return uuid;
-        }
-
-        public void setUuid(String uuid) {
-            this.uuid = uuid;
-        }
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getSex() {
-            return sex;
-        }
-
-        public void setSex(String sex) {
-            this.sex = sex;
-        }
-
-        public Integer getAge() {
-            return age;
-        }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        @Override
-        public String toString() {
-            return "Message{" +
-                    "uuid='" + uuid + '\'' +
-                    ", userName='" + userName + '\'' +
-                    ", password='" + password + '\'' +
-                    ", sex='" + sex + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
     }
 }
